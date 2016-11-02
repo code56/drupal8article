8c90819025d582605822e608064f02f5364a8b9f
code56 <evanthiak432@gmail.com>
2016-11-02 20:47:36 +0000

    trying to add js doesn't work yet just documenting the steps that dont work

---
+++ src/themes/ifigs_theme/js/scripts.js
@@ -0,0 +1,21 @@
+/*(function ($) {
+	Drupal.behaviors.myModuleBehavior = { 
+		attach: function (context, settings) { 
+			$('h1').css('color', 'red'); 
+		}
+	};
+})(jQuery);*/
+
+
+$(document).ready(function () {
+
+	alert("Hello world added js in the barktic.info!");  //here we can add our JS code
+
+	});
+/*
+(function ($) {
+  $(document).ready(function(){
+    alert("Hello world added js in the barktic.info!");  //here we can add our JS code
+  })
+})(jQuery);
+*/
