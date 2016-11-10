36932ccac0819de160f988615965760ca74966ec
code56 <evanthiak432@gmail.com>
2016-11-10 12:55:13 +0000

    trying to add this library with the flurp.js with drupal behaviour

--- src/themes/new_theme/js/flurp.js
+++ src/themes/new_theme/js/flurp.js
@@ -1,1 +1,16 @@
+/*$(document).ready(function () {
+
 alert("flurp.js is loaded");
+
+});
+*/
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.awesome = {
+    attach: function(context, settings) {
+      $('main', context).once('awesome').append('<p>Hello world</p>');
+    }
+  };
+
+}(jQuery));
