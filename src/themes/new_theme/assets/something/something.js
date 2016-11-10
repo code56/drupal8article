ccc5c27e99dcdcb7d006d54a71f467003e96cb3a
code56 <evanthiak432@gmail.com>
2016-11-10 11:02:41 +0000

    still trying to add this something.js script to the theme, i added the jquery dependency

---
+++ src/themes/new_theme/assets/something/something.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function(){
+	alert("Hello world added js in the new_theme custom theme!");  //here we can add our JS code
+	})
+});*/
+
+
+(function ($) {
+  $(document).ready(function(){
+    alert("Hello world added js in the new_theme custom theme!");  //here we can add our JS code
+  })
+})(jQuery);
