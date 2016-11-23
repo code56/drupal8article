5c1bf0f13625c731d9a07df29e1fda9aede84baf
code56 <evanthiak432@gmail.com>
2016-11-23 16:53:56 +0000

    added another asset library in the effort to have the bio-vis-expression-bar-working

--- src/themes/new_theme/js/applicationbiovisexpressionbar.js
+++ src/themes/new_theme/js/applicationbiovisexpressionbar.js
@@ -1,8 +1,7 @@
 (function ($) {
 
 $(document).ready(function(){
-    alert("Hello world added js in the theme, I am the flurp.js script being called!");  //here we can add our JS code
-   
+   alert("application-bio-vis.js is also being loaded hello!!!!!!!!!");  //
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = biovisexpressionbar = require("bio-vis-expression-bar");
 
@@ -70614,6 +70613,4 @@ $(document).ready(ready);
 $(document).on('page:load', ready);
 
   })
-
 }(jQuery));
-
